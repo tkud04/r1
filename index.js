3e3cae820139e11efa1ca092b68ad71e4c01a803
Tkud <kudayisitobi@gmail.com>
2021-08-27 12:03:21 +0100

    scrubbing

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+
+
+app.engine('.html', require('ejs').__express);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'html');
+
+
+app.get('/', (req, res) => {
+   res.render('index', {title: "Welcome", error: null});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
